2912f02911f8a3287ff4b0c0f6f26f132f432945
Brant Young <anatman.xx@gmail.com>
2014-11-19 11:42:46 +0800

    re-format source code.

--- src/com/glassky/main/MainActivity.java
+++ src/com/glassky/main/MainActivity.java
@@ -13,6 +13,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -32,69 +33,66 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		openCamera=(Button)findViewById(R.id.openCamera);
-		scanResult=(TextView)findViewById(R.id.scanResult);
-		inputContent=(EditText)findViewById(R.id.inputContent);
-		genCode=(Button)findViewById(R.id.genCode);
-		codeImg=(ImageView)findViewById(R.id.codeImg);
-		
-		openCamera.setOnClickListener(new OnClickListener(){
+
+		Log.i("MainActivity.onCreate", "onCreate event");
+
+		openCamera = (Button) findViewById(R.id.openCamera);
+		scanResult = (TextView) findViewById(R.id.scanResult);
+		inputContent = (EditText) findViewById(R.id.inputContent);
+		genCode = (Button) findViewById(R.id.genCode);
+		codeImg = (ImageView) findViewById(R.id.codeImg);
+
+		openCamera.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
-                Intent openCameraIntent = new Intent(MainActivity.this,CaptureActivity.class);  
-                startActivityForResult(openCameraIntent, 0); 
-			}			
+				Intent openCameraIntent = new Intent(MainActivity.this, CaptureActivity.class);
+				startActivityForResult(openCameraIntent, 0);
+			}
 		});
-		
-		genCode.setOnClickListener(new OnClickListener(){
+
+		genCode.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				String text=inputContent.getText().toString();
-				if(null!=text && !text.equals("")){
+				String text = inputContent.getText().toString();
+				if (null != text && !text.equals("")) {
 					try {
-			            QRCodeWriter writer = new QRCodeWriter();  
-			            Hashtable<EncodeHintType, String> hints = new Hashtable<EncodeHintType, String>();  
-			            hints.put(EncodeHintType.CHARACTER_SET, "utf-8");  
-						BitMatrix bitMatrix = writer.encode(text, BarcodeFormat.QR_CODE,  
-						        300, 300, hints);
-			            int[] pixels = new int[300 * 300];  
-			            for (int y = 0; y < 300; y++) {  
-			                for (int x = 0; x < 300; x++) {  
-			                    if (bitMatrix.get(x, y)) { 
-			                        pixels[y * 300 + x] = 0xff000000;  
-			                    } else {
-			                        pixels[y * 300 + x] = 0xffffffff;  
-			                    }  
-			  
-			                }  
-			            } 
-			            Bitmap bitmap = Bitmap.createBitmap(300, 300,  
-			                    Bitmap.Config.ARGB_8888); 
-			            bitmap.setPixels(pixels, 0, 300, 0, 0, 300, 300);
-			            
-			            codeImg.setImageBitmap(bitmap);
-			            
+						QRCodeWriter writer = new QRCodeWriter();
+						Hashtable<EncodeHintType, String> hints = new Hashtable<EncodeHintType, String>();
+						hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
+						BitMatrix bitMatrix = writer.encode(text, BarcodeFormat.QR_CODE, 300, 300, hints);
+						int[] pixels = new int[300 * 300];
+						for (int y = 0; y < 300; y++) {
+							for (int x = 0; x < 300; x++) {
+								if (bitMatrix.get(x, y)) {
+									pixels[y * 300 + x] = 0xff000000;
+								} else {
+									pixels[y * 300 + x] = 0xffffffff;
+								}
+
+							}
+						}
+						Bitmap bitmap = Bitmap.createBitmap(300, 300, Bitmap.Config.ARGB_8888);
+						bitmap.setPixels(pixels, 0, 300, 0, 0, 300, 300);
+
+						codeImg.setImageBitmap(bitmap);
+
 					} catch (WriterException e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
-					} 
+					}
 				}
-			}			
+			}
 		});
 	}
-	
-	@Override  
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {  
-        super.onActivityResult(requestCode, resultCode, data);  
-        if (resultCode == RESULT_OK) {  
-            Bundle bundle = data.getExtras();  
-            String result = bundle.getString("result");  
-            scanResult.setText(result);  
-        }  
-    } 
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if (resultCode == RESULT_OK) {
+			Bundle bundle = data.getExtras();
+			String result = bundle.getString("result");
+			scanResult.setText(result);
+		}
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -102,5 +100,4 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
-
 }
